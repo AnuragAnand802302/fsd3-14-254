b47898bc55959487a261fd8e5323716865290b84
Anurag Anand <prafuljuly30@gmail.com>
2026-07-23 16:25:57 +0530

    B

---
+++ LAB1/EventLoop/task3.js
@@ -0,0 +1,6 @@
+console.log("1.start");
+setTimeout(()=>console.log("2. setTimeout"), 1);
+setImmediate(()=>console.log("3. setImmediate"));
+process.nextTick(()=>console.log("4: nextTick"));
+console.log("5: end");
+//In most process Timeout and Immediate output sequence is Unpredictable.
