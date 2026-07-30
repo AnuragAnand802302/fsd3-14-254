cf5086345b1513729bbdf941e1c33b5d368dc266
Anurag Anand <prafuljuly30@gmail.com>
2026-07-30 16:31:23 +0530

    C2

---
+++ LAB2/cartProject.js
@@ -0,0 +1,77 @@
+import readline from "readline/promises";
+import { stdin, stdout } from "process";
+import { readFile, writeFile } from "fs/promises";
+//database using file starts;
+
+const File = "product.json";
+const getCart = async () => {
+  const data = await readFile(File, "utf-8");
+  return JSON.parse(data);
+};
+const saveCart = async (cart) => {
+  await writeFile(File, JSON.stringify(cart, null, 2)); //cart is variable name which contains product data, null is used to filter attributes and 2 is space in between two datas
+};
+const addToCart = async (product) => {
+  const cart = await getCart();
+  const isFoundInCart = cart.find((item) => item.id === product.id);
+  if (isFoundInCart) {
+    isFoundInCart += 1;
+  } else {
+    cart.push(product);
+    await saveCart(cart);
+    console.log(`${product.name} added/updated to 🛒`);
+  }
+};
+
+const displayCart = async () => {
+  const cart = await getCart();
+  if (cart.length == 0) {
+    console.log("🛒 is empty");
+    return;
+  }
+  console.table(cart);
+  const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0);
+  console.log(`Total payable amount Rs. ${total}`);
+};
+
+const main = async () => {
+  let choice;
+  const cin = readline.createInterface({ input: stdin, output: stdout });
+  do {
+    console.log("Welcome to Amazon Shopping🛒");
+    console.log("1 ......... Show Cart");
+    console.log("2 ......... Add Product");
+    console.log("3 .........  Remove Product");
+    console.log("4 ......... Update Product");
+    console.log("5 ......... CheckOut");
+    choice = await cin.question("enter your choice? ");
+    switch (Number(choice)) {
+      case 1:
+        await displayCart();
+        break;
+      case 2:
+        const item = await cin.question("enter Id, name, price, qty:");
+        const [id, name, price, qty] = item.split(",").map((p) => p.trim());
+        await addToCart({
+          id: Number(id),
+          name,
+          price: Number(price),
+          qty: Number(qty),
+        });
+        break;
+      case 3:
+        console.log("Remove Product");
+        break;
+      case 4:
+        console.log("Update Product");
+        break;
+      case 5:
+        console.log("CheckOut");
+        break;
+      default:
+        console.log("invalid Choice");
+    }
+  } while (choice != 5);
+  cin.close();
+};
+main();
