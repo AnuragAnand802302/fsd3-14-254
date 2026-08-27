cbedf319c309fa8ca678a3a5a5a194be79cbf320
Anurag Anand <prafuljuly30@gmail.com>
2026-08-27 14:01:44 +0530

    updated prg8

--- LAB3/prg8.js
+++ LAB3/prg8.js
@@ -1,10 +1,19 @@
 import http from "http"
 import { createReadStream } from "fs";
+import { writeFile, readFile, appendFile } from 'fs/promises';
 
 const server = http.createServer(async(req,res)=>{
-    const stream = createReadStream("big.txt", {encoding});
-    stream.pipe(res);
-    res.end();
+    if(req.url === '/stream'){
+        const stream = createReadStream("big.txt", {encoding: "utf-8"});
+        stream.pipe(res);
+    }else if(req.url === '/normal'){
+        res.write("Loading....");
+        const text = await readFile("big.txt");
+        res.end(text);
+    }else{
+
+    }
+    // res.end();
 });
 
 server.listen(3000,()=>console.log("Server is running..."));
