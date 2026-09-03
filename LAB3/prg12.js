1c47c9782ad3ced50f91fd0a98108f22de9af885
Anurag Anand <prafuljuly30@gmail.com>
2026-09-03 14:52:52 +0530

    updated prg12

--- LAB3/prg12.js
+++ LAB3/prg12.js
@@ -5,9 +5,44 @@ const server = http.createServer((req,res)=>{
     if(req.url === '/' && req.method === 'GET'){
         res.end("Home Page");
     }else if(req.url === "/product" && req.method === "GET"){
-        res.end("Show Product");
+        const products = [
+            {
+                id:1,
+                name:'mobile',
+                price:'2000',
+            },
+            {
+                id : 2,
+                name : 'laptop',
+                price : '20000',
+            },
+        ];
+
+        res.end(JSON.stringify(products));
+
     }else if(req.url === "/product" && req.method === "POST"){
-        res.end("add product");
+        // retrieve data from  client 
+        let body = "";
+        req.on("data", (chunk)=>{
+            body += chunk;
+        });
+
+        req.on("end",()=>{
+            const product = JSON.parse(body)
+            // add data to database
+            res.writeHead(201, {
+                "content-type":'application/json',
+
+            });
+            // send back the status
+            res.end(
+                JSON.stringify({
+                    msg:'product added',
+                    product,
+                }),
+            );
+        });
+        
     }else if(req.url === '/product' && req.method === "PUT"){
         res.end("Update Product");
     }else if(req.url === '/product' && req.method === "DELETE"){
